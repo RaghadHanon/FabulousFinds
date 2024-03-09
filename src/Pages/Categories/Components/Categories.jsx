3d73c2802dc776018e79809dbb7f571f6c986f10
RaghadHanon <raghadhanoon2015@gmail.com>
2024-03-09 22:42:11 +0200

    add categories section

--- src/Pages/Categories/Components/Categories.jsx
+++ src/Pages/Categories/Components/Categories.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import CategoriesSection from '../../Home/Components/CategoriesSection/CategoriesSection'
 
 function Categories() {
   return (
-    <div>Categories</div>
+    <CategoriesSection/>
   )
 }
 
